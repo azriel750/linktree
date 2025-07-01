10ec9cdd274df0342dd8a4efc812df25558d17d5
azriel750 <marc.ubertalli.simplon@gmail.com>
2025-07-01 13:35:59 +0200

    version avec image ajuster

---
+++ script.js
@@ -0,0 +1,7 @@
+for (let index = 0;index <Data.length;index++){
+    console.og(data[index]);
+    if (data.index === 4){
+console.log ("le Prénom fait plus de caractère")
+    }else { console.log ("le prenom fait moins de caractère")
+}
+}
